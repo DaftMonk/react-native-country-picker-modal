cf0f18f61e610c2d03f325912cd1f0233f7d9d63
tyhenkel <tyhenkel@gmail.com>
2024-10-03 23:03:33 -0400

    update

--- src/index.tsx
+++ src/index.tsx
@@ -50,7 +50,7 @@ interface Props {
   onClose?(): void
 }
 
-const Main = ({ theme, translation, withEmoji = true, onSelect = () => {}, ...props }: Props) => {
+const Main = ({ theme, translation, withEmoji = true, ...props }: Props) => {
   return (
     <ThemeProvider theme={{ ...DEFAULT_THEME, ...theme }}>
       <CountryProvider value={{ ...DEFAULT_COUNTRY_CONTEXT, translation }}>
